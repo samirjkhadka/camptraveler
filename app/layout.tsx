b45ae51768afa3d938770fc895c1ec634cd9f925
samirjkhadka <findsamirkhadka@gmail.com>
2024-08-23 16:58:23 +0545

    Navbar complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Camp Traveler",
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden"> {children}</main>
+
+        <Footer />
+      </body>
     </html>
   );
 }
