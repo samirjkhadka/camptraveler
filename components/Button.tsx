430bfc185bf8f53fb5d97a9af741aaffe61343a4
samirjkhadka <findsamirkhadka@gmail.com>
2024-08-24 16:20:14 +0545

    call to action section complete

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,10 +9,10 @@ type ButtonProps = {
   full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant,full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} &{full && "w-full"}`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
